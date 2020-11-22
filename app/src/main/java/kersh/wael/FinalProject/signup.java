73a23b42925bd8e37eb64b2a441f07b52a2f47c5
wael <wael.kk.23@gmail.com>
2020-11-23 01:23:33 +0200

    Initial commit

---
+++ app/src/main/java/kersh/wael/FinalProject/signup.java
@@ -0,0 +1,90 @@
+package kersh.wael.FinalProject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        etEmail = findViewById(R.id.etEmail);
+        etPhone = findViewById(R.id.etPhone);
+        etPassword = findViewById(R.id.etPassword);
+        etPassword2 = findViewById(R.id.etPassword2);
+        etFirstName = findViewById(R.id.etFirstName);
+        etLastName = findViewById(R.id.etLastName);
+        btnsave = findViewById(R.id.btnSave);
+        btnsave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {//class without name
+                checkForm();
+
+            }
+        });
+    }
+
+    /**
+     * המתודה בודקת תקינות טופם ההרשמה ואם תקין מבצעת הרשמה\The method checks the correctness of the registration top and whether it is correct performs the registration
+     */
+    private void checkForm() {
+        String PhoneNumber = etPhone.getText().toString();
+        String Email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+        String password2 = etPassword2.getText().toString();
+        String FirstName = etFirstName.getText().toString();
+        String LastName = etLastName.getText().toString();
+
+        boolean isOk = true;
+
+
+        if (FirstName.length() < 2) {
+            isOk = false;
+            etFirstName.setError("at least two letters");
+        }
+        if (LastName.length() < 2) {
+            isOk = false;
+            etLastName.setError("at least two letters");
+        }
+        if (Email.length() < 5 || Email.indexOf('@') == 0 || Email.indexOf('@') >= Email.length() - 2 ||
+                Email.indexOf('.') == 0 || Email.indexOf('.') >= Email.length() - 1 || Email.lastIndexOf('.') < Email.indexOf('@')) {
+            isOk = false;
+            etEmail.setError("wrong Email syntax");
+        }
+        if (password.equals(password2) == false) {
+            isOk = false;
+            etPassword.setError("Password must ne the same");
+        } else {
+            //MyValidation myValidations=new MyValidations();
+            if (password.length() < 8 || password.indexOf(' ') >= 0) {
+                isOk = false;
+                etPassword.setError("Invalid Password");
+            }
+        }
+        if (isOk)// isOk = true
+        {
+            // todo: create account and return to sign in screen/ close this screen
+            createNewAccount(Email, FirstName, LastName, password, PhoneNumber);
+        }
+
+    }
+
+    /**
+     * @param gmail
+     * @param firstName
+     * @param lastName
+     * @param password
+     * @param phone
+     */
+
+    private void createNewAccount(String gmail, String firstName, String lastName, String password, String phone) {
+        FirebaseAuth auth = FirebaseAuth.getInstance(); // אחראית על רישום וכניסת משתמשים
+    }
+
+
+
+}
+}
